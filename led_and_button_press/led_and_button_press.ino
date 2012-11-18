f3bbca3228cdbb29a38d21f7cf0fa06287427ea4
Joseph W. Breu <breu@breu.org>
2012-11-18 15:33:29 -0600

    add more recursive functions and move the button to pin 2

--- led_and_button_press/led_and_button_press.ino
+++ led_and_button_press/led_and_button_press.ino
@@ -1,11 +1,11 @@
 // Recrsive lights
 // Joe Breu (breu@breu.org)
 //
-// LEDs attached to pins 2-9
+// LEDs attached to pins 3-10
 // Button on pin 13
 
-int ledPins[] = {2,3,4,5,6,7,8,9};
-const int buttonPin = 13;
+int ledPins[] = {3,4,5,6,7,8,9,10};
+const int buttonPin = 2;
 int buttonState = 0;
 
 void setup()
@@ -20,20 +20,52 @@ void loop()
 {
   buttonState = digitalRead(buttonPin);
   if (buttonState == LOW) {
-    light_it_up(3,4,50);
+    light_it_up_center(3,4,75);
+    delay(100);
+    light_one_up(0,1,75);
+    delay(100);
+    light_one_up(7,-1,75);
+    delay(100);
+    light_it_up_outside(0,7,75);
+    delay(100);
+
   }
 }
 
 
-void light_it_up(int left, int right, int delayTime)
+void light_it_up_center(int left, int right, int delayTime)
 {
   if (left < 0 || right > 8)             // check if we are out of bounds
     return;
   digitalWrite(ledPins[left],HIGH);      // turn on the left light
   digitalWrite(ledPins[right],HIGH);     // turn on the right light 
   delay(delayTime);
-  light_it_up(left-1,right+1,delayTime); // go recursive
+  light_it_up_center(left-1,right+1,delayTime); // go recursive
   digitalWrite(ledPins[left],LOW);       // turn off the left light
   digitalWrite(ledPins[right],LOW);      // turn off the right light
+  delay(delayTime);
+}
+
+void light_it_up_outside(int left, int right, int delayTime)
+{
+  if (right < left)
+    return;
+  digitalWrite(ledPins[left],HIGH);      // turn on the left light
+  digitalWrite(ledPins[right],HIGH);     // turn on the right light 
+  delay(delayTime);
+  light_it_up_outside(left+1,right-1,delayTime); // go recursive
+  digitalWrite(ledPins[left],LOW);       // turn off the left light
+  digitalWrite(ledPins[right],LOW);      // turn off the right light
+  delay(delayTime);
+}
+
+void light_one_up(int index, int delta, int delayTime)
+{
+  if ( index < 0 || index > 8)          // check if we are out of bounds
+    return;
+  digitalWrite(ledPins[index],HIGH);
+  delay(delayTime);
+  light_one_up(index+delta,delta,delayTime);
+  digitalWrite(ledPins[index],LOW);
   delay(delayTime);
 }
